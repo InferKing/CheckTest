f3bbb1931f969a12209b107cc2fa22c9bb116f01
InferKing <viacinoleg2012@gmail.com>
2022-02-11 00:29:45 +0500

    fixed bugs

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -5,4 +5,17 @@ using UnityEngine;
 public class BallBehaviour : MonoBehaviour
 {
     public bool isInPlace = true;
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        float y = rb.velocity.y;
+        if (rb.velocity != Vector3.zero)
+        {
+            rb.velocity = new Vector3(0, y, 0);
+        }
+    }
 }
